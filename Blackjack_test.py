ce3430cd66effe07f9fc663b3d60e90331b3ea76
Nomad-cypher <jenshjepsen@gmail.com>
2026-02-11 15:16:39 +0100

    Unit tests are working now :D Apparantly functions for unit tests are supposed to be written test_functionname and not functionname_test I think that's stupid. Well, at least I got my unit tests working now, so I can look into later if there's some setting I can change so I can use functionname_test instead.

--- Blackjack_test.py
+++ Blackjack_test.py
@@ -7,32 +7,38 @@ class test_test(unittest.TestCase): #This is just to see if unit testing is actu
         self.assertEqual("one","one")
 
 class reset_game_test(unittest.TestCase):
-    def reset_test(self): #Test if the reset happens properly
+    def test_reset(self): #Test if the reset happens properly
         reset_game()
         self.assertEqual(playerHand, []) #Player hand should be empty
         self.assertEqual(dealerHand, []) #Dealer hand should be empty
-        self.assertEqual(deck.sort, list(range(1,52+1))) #Deck should contain each card exactly once
+        deck.sort()
+        self.assertEqual(deck, list(range(1,52+1))) #Deck should contain each card exactly once
 
 class read_card_from_index_test(unittest.TestCase):
-    def card_test(self): #Test if the function returns the right cards
+    def test_cards(self): #Test if the function returns the right cards
         self.assertEqual(read_card_from_index(1), "Ace of Spades")
         self.assertEqual(read_card_from_index(13+12), "Queen of Hearts")
         self.assertEqual(read_card_from_index(26+13), "King of Clubs")
-        self.assertEqual(read_card_from_index(39+11), "Jack of Clubs")
+        self.assertEqual(read_card_from_index(39+11), "Jack of Diamonds")
         self.assertEqual(read_card_from_index(10), "10 of Spades")
         self.assertEqual(read_card_from_index(13+6), "6 of Hearts")
 
     #TODO: Tests for unexpected inputs
 
 class calculate_points_test(unittest.TestCase):
-    def hands_test(self): #Test if various hands returns the expected amount of points
+    def test_hands(self): #Test if various hands returns the expected amount of points
         self.assertEqual(calculate_points([26]), 10) #King of hearts
         self.assertEqual(calculate_points([1]), 11) #One Ace
         self.assertEqual(calculate_points([1, 14]), 12) #Two Aces
         self.assertEqual(calculate_points([1, 14, 27]), 13) #Three Aces
-        self.assertEqual(calculate_points([1, 24, 27, 40]), 14) #Four Aces
+        self.assertEqual(calculate_points([1, 14, 27, 40]), 14) #Four Aces
         self.assertEqual(calculate_points([8, 14]), 19) #8 and one Ace
         self.assertEqual(calculate_points([8, 14, 41]), 21) #Drawing a 2 should take the score to 21
         self.assertEqual(calculate_points([8, 14, 42]), 12) #Drawing a 3 should take the score to 12, since now the Ace counts as 1 point
 
     #TODO: Tests for unexpected inputs
+
+'''
+if __name__ == "__main__": #This did nothing
+    unittest.main()
+'''
